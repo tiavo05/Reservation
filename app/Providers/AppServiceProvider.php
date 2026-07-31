4795f08ee85c045319dfa191c765f43166fadb39
Tiavo <tiavo@localhost.localdomain>
2026-07-31 10:33:35 -0400

    Fix URL facade import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,24 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-            if (app()->environment('production')) {
-                URL::forceScheme('https');
-            }
+        URL::forceScheme('https');
     }
 }
